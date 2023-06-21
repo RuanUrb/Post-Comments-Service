264ce8913508965957df1e4b2f9d1649220264df
RuanUrb <ruaurb@alunos.utfpr.edu.br>
2023-06-21 18:35:29 -0300

    New moderation service added. Filter to words added

--- eventbus/index.js
+++ eventbus/index.js
@@ -7,9 +7,11 @@ app.use(bodyParser.json())
 
 app.post('/events', (req, res)=>{
     const event = req.body
-    axios.post('http://localhost:4000/events', event)
-    axios.post('http://localhost:4001/events', event)
-    axios.post('http://localhost:4002/events', event)
+    axios.post('http://localhost:4000/events', event)   //post service
+    axios.post('http://localhost:4001/events', event)  //comment service
+    axios.post('http://localhost:4002/events', event) //query service
+    axios.post('http://localhost:4003/events', event)//moderation service
+
     res.send({status: 'OK'})
 })
 
